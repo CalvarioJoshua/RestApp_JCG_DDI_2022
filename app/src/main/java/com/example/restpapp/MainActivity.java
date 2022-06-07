0d9b5f329a3afe756bfd637d7b6e0390aa9c6efb
CalvarioJoshua <utp0010593@alumno.utpuebla.edu.mx>
2022-06-07 11:58:04 -0500

    Agregada Lista msj

--- app/src/main/java/com/example/restpapp/MainActivity.java
+++ app/src/main/java/com/example/restpapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.restpapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.restpapp.databinding.ActivityMainBinding;
@@ -9,15 +10,19 @@ import com.example.restpapp.databinding.ActivityMainBinding;
 public class MainActivity extends Activity {
 
     private TextView mTextView;
-    private ActivityMainBinding binding;
+    private Button mButton;
+    private final List <String>mensajes = Arrays.asList(
+    "Buenos días","Hola Como estas","Buenas las Tengas","Bye Bye","Cuidate Mucho","Holis","Buenas",
+    "Adios","Que tranza","Quioñaquioña");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        mTextView = findViewById(R.id.MsjTextView);
+        mButton = findViewById(R.id.Button);
 
-        mTextView = binding.text;
+        mTextView.setText(mensajes.get[1]);
+        mTextView.setText(mensajes);
     }
 }
